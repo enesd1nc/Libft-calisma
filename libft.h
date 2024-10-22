a76ddf03984b3068774737e3e7d76a1ed758bc1e
Enes <mehmetenes2004.dinc@gmail.com>
2024-10-22 10:21:57 +0300

    42

--- libft.h
+++ libft.h
@@ -13,9 +13,7 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# include <stdio.h>
-# include <string.h>
-# include <unistd.h>
+
 # include <stdlib.h>
 
 int	ft_isalpha(int c);
--- main.c
+++ main.c
@@ -1,21 +1,11 @@
-#include <stdio.h>
 #include "libft.h"
+#include <stdio.h>
+void	test_isalpha()
+{
+	printf("%d\n",ft_isalpha(65));
+}
 
 int main(void)
 {
-    char src[] = "Hello, 42!";
-    char dest[20];
-    size_t copied_length;
-
-    printf("Testing ft_strlcpy:\n");
-
-    copied_length = ft_strlcpy(dest, src, sizeof(dest));
-    printf("Source String: %s\n", src);
-    printf("Destination String: %s\n", dest);
-    printf("Length of Source: %zu\n", copied_length);
-    printf("\n");
-
-    return 0;
+	test_isalpha();
 }
-
-
